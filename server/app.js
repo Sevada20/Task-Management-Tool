fbeea051955f0101da1ca9d820ac18ff6f65c893
Sevada20 <poxosyansevada@gmail.com>
2025-02-03 02:41:07 +0400

    a

--- server/app.js
+++ server/app.js
@@ -17,6 +17,9 @@ app.use(
     origin: [
       "https://task-management-tool-git-main-sevada20s-projects.vercel.app",
       "http://localhost:5173",
+      "http://localhost:5174",
+      "http://localhost:5175",
+      "http://localhost:3000",
     ],
     credentials: true,
   })
