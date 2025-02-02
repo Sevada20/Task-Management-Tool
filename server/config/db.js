61df1727a1cd85680ec222ef4c62257e48b33b80
Sevada20 <poxosyansevada@gmail.com>
2025-02-02 17:28:40 +0400

    no change

--- server/config/db.js
+++ server/config/db.js
@@ -16,6 +16,7 @@ const connectDB = async () => {
     console.log("MongoDB connected");
   } catch (error) {
     console.error("Database connection error:", error);
+
     process.exit(1);
   }
 };
