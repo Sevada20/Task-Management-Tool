4cb81172e275f1e9a1b732de639ac2e027984c2a
Sevada20 <poxosyansevada@gmail.com>
2025-02-03 02:16:21 +0400

    no change

--- src/pages/LoginPage/styles.ts
+++ src/pages/LoginPage/styles.ts
@@ -1,7 +1,7 @@
 import { createUseStyles } from "react-jss";
 
 const useStyles = createUseStyles(
-  (theme) => ({
+  () => ({
     authContainer: {
       height: "100vh",
       maxWidth: "30vw",
--- src/utils/dateUtils.ts
+++ src/utils/dateUtils.ts
@@ -1,6 +1,5 @@
 // function to format date to the format "DD.MM.YYYY" [S.P]
 export const formatDate = (dateString: string) => {
   const options = { year: "numeric", month: "long", day: "numeric" };
-  // @ts-ignore
-  return new Date(dateString).toLocaleDateString(undefined, options);
+  return new Date(dateString).toLocaleDateString(undefined, options as any);
 };
