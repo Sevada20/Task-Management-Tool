3e95d2da8b7aa575f9279f1733d12b1175d65b04
Sevada20 <poxosyansevada@gmail.com>
2025-02-02 23:15:33 +0400

    no change

--- server/config/db.js
+++ server/config/db.js
@@ -18,6 +18,8 @@ const connectDB = async () => {
   } catch (error) {
     console.error("Database connection error:", error);
     process.exit(1);
+  } finally {
+    console.log("Database connection closed");
   }
 };
 
