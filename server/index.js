49240588384ef9c2f791c65db609a0d292943bad
Sevada20 <poxosyansevada@gmail.com>
2025-02-03 13:10:09 +0400

    added cors

--- server/index.js
+++ server/index.js
@@ -6,6 +6,11 @@ app.use(
       "http://localhost:5173",
       "http://localhost:5174",
       "http://localhost:5175",
+      "https://task-management-tool-git-main-sevada20s-projects.vercel.app",
+      "https://task-management-tool-khaki.vercel.app/api",
+      "https://task-management-tool-beige-rho.vercel.app",
+      "https://task-management-tool-server.vercel.app/api",
+      "http://localhost:3000",
     ],
     credentials: true,
   })
