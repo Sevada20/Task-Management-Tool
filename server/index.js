80535d08ca732d6f23aa7018624784f04b5d9429
Sevada20 <poxosyansevada@gmail.com>
2025-02-03 15:32:24 +0400

    no changes

--- server/index.js
+++ server/index.js
@@ -1,13 +1,37 @@
-const cors = require("cors");
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import { authenticate } from "./middleware/auth.js";
+import authRoutes from "./routes/auth.js";
+import taskRoutes from "./routes/tasks.js";
+import userRoutes from "./routes/users.js";
+import connectDB from "./config/db.js";
 
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
 app.use(
   cors({
     origin: [
       "http://localhost:5173",
+      "https://deploy-mern-1whq.vercel.app",
       "http://localhost:5174",
       "http://localhost:5175",
-      "http://localhost:3000",
     ],
+    methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
 );
+
+connectDB();
+
+app.use("/api/auth", authRoutes);
+app.use("/api/tasks", authenticate, taskRoutes);
+app.use("/api/users", authenticate, userRoutes);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+export default app;
