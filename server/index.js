9bb87cd022cea3775b9ecb20745bb0b1d2bdb920
Sevada20 <poxosyansevada@gmail.com>
2025-02-04 17:40:23 +0400

    a

--- server/index.js
+++ server/index.js
@@ -16,7 +16,6 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "https://deploy-mern-1whq.vercel.app",
       "http://localhost:5174",
       "http://localhost:5175",
     ],
@@ -31,7 +30,6 @@ app.use("/api/auth", authRoutes);
 app.use("/api/tasks", authenticate, taskRoutes);
 app.use("/api/users", authenticate, userRoutes);
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(5000, () => console.log(`Server running on port ${PORT}`));
 
 export default app;
